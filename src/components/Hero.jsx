9236679ead091b7a390e75a00cb4a87259513953
VamshiReddy02 <vamshiproject02@gmail.com>
2025-04-19 15:37:43 +0530

    added NavBar
    Signed-off-by: VamshiReddy02 <vamshiproject02@gmail.com>

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Hero = () => {
+  return (
+    <section className='bg-red-400'>
+        <Navbar />
+        <div className="container grid grid-cols-1 md:grid-cols-2 h-screen md:h-[700px] relative">
+           {/*Info*/}
+           {/*Image*/}
+        </div>
+    </section>
+  )
+}
+
+export default Hero
